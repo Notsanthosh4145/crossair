897f12f4bf6638bf1b28d437806eebf169586d98
Notsanthosh4145 <144212279+Notsanthosh4145@users.noreply.github.com>
2026-09-12 23:22:53 +0530

    Update settings.py

--- crossair_backend/backend/settings.py
+++ crossair_backend/backend/settings.py
@@ -49,9 +49,9 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',  
-    'corsheaders.middleware.CorsMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
